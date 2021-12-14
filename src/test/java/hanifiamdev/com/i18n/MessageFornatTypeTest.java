612ed9df91b6408fba3a20c78257c4c9ca1c57c9
hanifiamdev <hanifamrullahdev@gmail.com>
2021-12-14 13:37:17 +0700

    Message Format Type

---
+++ src/test/java/hanifiamdev/com/i18n/MessageFornatTypeTest.java
@@ -0,0 +1,45 @@
+package hanifiamdev.com.i18n;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MessageFornatTypeTest {
+
+    @Test
+    void testMessageFormatTypeIndonesia() {
+        var locale = new Locale("in", "ID");
+        var resourceBundle = ResourceBundle.getBundle("message", locale);
+
+        var pattern = resourceBundle.getString("status");
+
+        var messageFormat = new MessageFormat(pattern, locale);
+
+
+        var format = messageFormat.format(new Object[]{
+                "Hanif", new Date(), 1000000
+        });
+
+        System.out.println(format);
+    }
+
+    @Test
+    void testMessageFormatTypeAmerica() {
+        var locale = new Locale("en", "US");
+        var resourceBundle = ResourceBundle.getBundle("message", locale);
+
+        var pattern = resourceBundle.getString("status");
+
+        var messageFormat = new MessageFormat(pattern, locale);
+
+
+        var format = messageFormat.format(new Object[]{
+                "Hanif", new Date(), 1000000
+        });
+
+        System.out.println(format);
+    }
+}
